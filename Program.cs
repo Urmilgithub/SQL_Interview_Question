9a92c44fc421f797ddcc2f50935bf1bf16b2e46e
Urmilgithub <urmilpatelit27@gmail.com>
2026-04-27 23:52:39 +0530

    Employees with salary greater than 30,000

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Find employees with salary greater than 30,000
+
+SELECT * 
+FROM Employee
+WHERE Salary > 30000;
