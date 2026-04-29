d3cc52e59fe23dfb959ba8695aa4cc39195d3f21
Urmilgithub <urmilpatelit27@gmail.com>
2026-04-29 23:34:33 +0530

    Departmentwise Salary (Window Function)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿//Find employees with salary greater than 30,000
 
+using System.Collections.Concurrent;
+
 SELECT * 
 FROM Employee
 WHERE Salary > 30000;
@@ -12,8 +14,17 @@ WHERE Salary BETWEEN 20000 AND 35000;
 
 
 // Nth Highest Salary (Window Function)
-SELECT* FROM;
+SELECT * FROM
 (
-    SELECT EMPLOYEE_Id, NAME, SALARY, DENSE_RANK() OVER (ORDER BY SALARY) AS SalaryRank
-    )t
+    SELECT EMPLOYEE_Id, NAME, SALARY, DENSE_RANK() OVER (ORDER BY SALARY DESC) AS SalaryRank
+   )t
     WHERE SalaryRank = 3;
+
+
+// Departmentwise Salary (Window Function)
+SELECT * FROM
+    (
+    SELECT EMPLOYEE_Id, Name, Salary, Dept_Id,
+    ROW_NUMBER() OVER (PARTITION BY ORDER BY SALARY DESC) AS DEPT_Sal
+    )t
+    WHERE DEPT_Sal = 3; 
